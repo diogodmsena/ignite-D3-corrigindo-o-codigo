725f109e2fdd57a6bae6cd0aee895d2b34d64c55
Diogo Sena <diogodmsena@gmail.com>
2021-06-02 21:20:28 -0300

    Challenge finished

--- src/index.js
+++ src/index.js
@@ -23,22 +23,28 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  const repository = repositories[repositoryIndex]
+  if (title)
+    repository.title = title
+  if (url)
+    repository.url = url
+  if (techs)
+    repository.techs = techs
 
   return response.json(repository);
 });
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +72,10 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const repository = repositories[repositoryIndex]
+  repository.likes++
 
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
